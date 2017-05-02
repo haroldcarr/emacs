547ac18b42afe226bbb56d74e685a8992fc9e97d
Harold Carr <harold.carr@gmail.com>
2017-05-01 17:42:52 -0700

    got elcim working again

--- .emacs.d.spacemacs/private/hc-config/packages.el
+++ .emacs.d.spacemacs/private/hc-config/packages.el
@@ -5,9 +5,9 @@
 (setq hc-config-packages
       '(
         eclim
+        company-emacs-eclim
         hindent
         intero
-        ;;gud
         markdown-preview-mode
         peep-dired
         ))
@@ -15,68 +15,93 @@
 ;; List of packages to exclude.
 (setq hc-config-excluded-packages '())
 
-;; ------------------------------------------------------------------------------
-;; ECLIPSE
-;; - installer
-;;   - turn off "Bundle Pools"
-;;   - Eclipse IDE for Java Developers
-;; - /Applications/Eclipse-4-6-3-neon.app/Contents/MacOS/eclipse &
+;; -----------------------------------------------------------------------------
+;;
+;; MANUALLY START ECLIPSE
+;;
+;; http://eclim.org/install.html
+;; - Download ECLIPSE NEON 64 BIT (full app - not installer)
+;; - mkdir -p /Volumes/HC/.sync/ftptmp/ECLIPSE/workspace
+;; - LAUNCH
+;;   - Workspace : /Volumes/HC/.sync/ftptmp/ECLIPSE/workspace
 ;; ECLIM
 ;; - graphic install : java -jar ~/Downloads/eclim_2.6.0.jar
-;;   - says : you can start eclimd server headless via /Applications/Eclipse-4-6-3-neon.app/Contents/Eclipse/eclimd
+
+;; USAGE: https://github.com/emacs-eclim/emacs-eclim/wiki
 ;;
-;; what is this for? : /Users/carr/Documents/workspace
+;; COMPLETION
+;;     M-`         / company-emacs-eclim
+;;         automatic completion as you type (requires some typing to kick in)
+;;             use key binding to show all possibilities before you type
+;;     M-TAB       / eclim-complete
+;;         displays buffer of completions
+;; LIST PROBLEMS
+;;     C-c C-e b   / eclim-problems
+;;         display errors/warnings
+;;             switch between errors/warnings via "e" and "w"
+;;             jump to source via RET
+;;             "g" to refresh
+;;     eclim-problems-compilation-buffer
+;;         compilation buffer with errors and warnings
+;;             move around via next-error (C-`) and prev-error
+;; FIND
+;;     M-.         / eclim-java-find-declaration
+;;         go to definition of identifier under point
+;;     C-c C-e f t / eclim-java-find-type
+;;          go to class definition by entering name
+;;     C-c C-e f r / eclim-java-find-references
+;;          display references to identifier under point
+;; IMPORTS
+;;     C-c C-e i   / eclim-java-import-organize
+;;          adds imports
+;;              Preferences -> Java -> Code Style -> Organize Imports
+;;                  catalog-service/etc/ides/eclipse-google-style.importorder
+;; CLASS HIERARCHY :
+;;     C-c C-e h   / eclim-java-hierarchy
+;;          display hierarchy tree for class in the current buffer
 
 (defun hc-config/init-eclim ()
   (use-package eclim
     :config
     (progn
-      (use-package eclimd)       ;; starts/stop clim from emacs
-      (use-package eclim-debug)  ;; debug integration with gdb/gud
-
       (global-eclim-mode)
       (custom-set-variables
-       '(eclim-eclipse-dirs '("/Applications/Eclipse-4-6-3-neon.app/Contents/Eclipse"))
-       '(eclim-executable     "/Applications/Eclipse-4-6-3-neon.app/Contents/Eclipse/eclim"))
-       ;;'(eclimd-autostart   t))
-      ;;(setq help-at-pt-display-when-idle t)
-      ;;(setq help-at-pt-timer-delay 0.1)
-      ;;(help-at-pt-set-timer)
+       '(eclim-eclipse-dirs '("/Applications/Eclipse.app/Contents/Eclipse"))
+       '(eclim-executable     "/Applications/Eclipse.app/Contents/Eclipse/eclim"))
+
+      (define-key eclim-mode-map (kbd "M-.")
+        (lambda ()
+          (interactive)
+          (eclim-java-find-declaration)
+          (recenter-top-bottom)))
+
+      (define-key eclim-mode-map (kbd "M-`") 'company-emacs-eclim)
+
+      ;; https://www.bountysource.com/issues/43113986-shouldn-t-problems-appear-in-the-minibuffer-when-you-move-your-cursor-over-them
+      (setq help-at-pt-display-when-idle t)
+      (setq help-at-pt-timer-delay 0.1)
+      (help-at-pt-set-timer)
 
       ;; Ulises eclim bindings
-      ;;(define-key eclim-mode-map (kbd "C-c C-e p b") 'eclim-project-build)
       ;;(define-key eclim-mode-map (kbd "C-c C-e C-f") 'eclim-problems-correct)
       ;;(define-key eclim-mode-map (kbd "C-c C-e C-p C-n") 'eclim-problems-next-same-window)
       ;;(define-key eclim-mode-map (kbd "C-c C-e C-p C-p") 'eclim-problems-previous-same-window)
-      ;; helm-eclim bindings
+
+      ;; Ulises helm-eclim bindings
       ;;(define-key eclim-mode-map (kbd "C-c C-e h") nil)
       ;;(define-key eclim-mode-map (kbd "C-c C-e h t") 'helm-eclim-java-find-type)
       ;;(define-key eclim-mode-map (kbd "C-c C-e h u") 'helm-eclim--update-cache)
       ;;(define-key eclim-mode-map (kbd "C-c C-e h m") 'helm-eclim-java-find-method)
       ;;(define-key eclim-mode-map (kbd "C-c C-e h r") 'helm-eclim-java-find-references)
-      ;;(define-key eclim-mode-map (kbd "M-.") (lambda ()
-      ;;                                         (interactive)
-      ;;                                         (eclim-java-find-declaration)
-      ;;                                         (recenter-top-bottom)))
-      ;;(define-key eclim-mode-map (kbd "M-,") 'pop-global-mark)
-
-      ;; paredit for java
-      ;;(add-hook 'java-mode-hook
-      ;;          (lambda () (setq eclim-project-name (eclim-project-name))))
-
-      ;; company stuff
-      ;;(use-package company)
-      ;;(use-package company-emacs-eclim)
-      ;;(company-emacs-eclim-setup)
-      ;;(global-company-mode t)
-
-      ;; gud-mode stuff
-      ;;(define-key eclim-mode-map (kbd "C-c C-a C-b") 'gud-break)
-      ;;(define-key eclim-mode-map (kbd "C-c C-a C-n") 'gud-next)
-      ;;(define-key eclim-mode-map (kbd "C-c C-a C-s") 'gud-step)
-      ;;(define-key eclim-mode-map (kbd "C-c C-a C-c") 'gud-cont)
-      ;;(define-key eclim-mode-map (kbd "C-c C-a C-r") 'gud-run)
       )))
+
+;; code completion: https://github.com/emacs-eclim/emacs-eclim/wiki/Code-Completion
+(defun hc-config/init-company-emacs-eclim ()
+  (use-package company-emacs-eclim
+    :config
+    (progn
+      (company-emacs-eclim-setup)
+      (global-company-mode t))))
 
 ;; ------------------------------------------------------------------------------
 
