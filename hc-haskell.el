857e3f80ee4b04b8f79d347f21895f85eb95b30f
Harold Carr <harold.carr@gmail.com>
2017-10-26 09:11:19 -0700

    add var to ask which of dante/intero I loaded

--- hc-haskell.el
+++ hc-haskell.el
@@ -4,16 +4,14 @@
 
 ;;;;
 ;;;; Created       : ...                        by Harold Carr.
-;;;; Last Modified : 2017 Oct 25 (Wed) 18:47:46 by Harold Carr.
+;;;; Last Modified : 2017 Oct 26 (Thu) 09:10:37 by Harold Carr.
 ;;;;
 
 ;;; Code:
 
 ;;; Haskell Packages
 
-(defvar hc-emacs-location (shell-command-to-string "hcLocation emacs"))
-
-;; https://github.com/cydparser/demo-emacs-haskell/blob/master/demo.org
+;; for Intero, see: https://github.com/cydparser/demo-emacs-haskell/blob/master/demo.org
 
 (use-package haskell-mode
   :ensure t
@@ -41,11 +39,15 @@
 ;;  :diminish ""
 ;;  :init (add-hook 'haskell-mode-hook #'hlint-refactor-mode))
 
-(cond ((y-or-n-p-with-timeout "Use Dante (otherwise use Intero)" 4 nil)
+(defvar hc-haskell)
+
+(cond ((y-or-n-p-with-timeout "Use Dante (otherwise use Intero, the default)" 6 nil)
        (message "Using Dante")
+       (setq hc-haskell 'dante)
        (use-package hc-haskell-dante))
       (t
        (message "Using Intero")
+       (setq hc-haskell 'intero)
        (use-package hc-haskell-intero)))
 
 (provide 'hc-haskell)
