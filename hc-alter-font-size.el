65ee5693c6874f825dfd32937cfbf6a29fccbd65
Harold Carr <harold.carr@gmail.com>
2018-05-18 16:09:56 -0700

    font size

--- hc-alter-font-size.el
+++ hc-alter-font-size.el
@@ -1,20 +1,40 @@
+;;; hc-alter-font-size --- font
+
+;;; Commentary:
+
+;;; Code:
+
+;; ------------------------------------------------------------------------------
+;; https://github.com/zonuexe/emacs-presentation-mode
+
+;; this one changes font size but keeps frame size the same
+
+(use-package presentation)
+
+;; ------------------------------------------------------------------------------
 ;; http://emacsninja.com/posts/making-emacs-more-presentable.html
 
-(defun my-alter-frame-font-size (fn)
+;; this one shrinks/enlarges frame
+
+(defun hc-alter-frame-font-size (fn)
   (let* ((current-font-name (frame-parameter nil 'font))
          (decomposed-font-name (x-decompose-font-name current-font-name))
-         (font-size (string-to-int (aref decomposed-font-name 5))))
+         (font-size (string-to-number (aref decomposed-font-name 5))))
     (aset decomposed-font-name 5 (int-to-string (funcall fn font-size)))
     (set-frame-font (x-compose-font-name decomposed-font-name))))
 
-(defun my-inc-frame-font-size ()
+(defun hc-inc-frame-font-size ()
   (interactive)
-  (my-alter-frame-font-size '1+))
+  (hc-alter-frame-font-size '1+))
 
-(defun my-dec-frame-font-size ()
+(defun hc-dec-frame-font-size ()
   (interactive)
-  (my-alter-frame-font-size '1-))
+  (hc-alter-frame-font-size '1-))
 
-;; (global-set-key (kbd "C-+") 'my-inc-frame-font-size)
-;; (global-set-key (kbd "C-=") 'my-inc-frame-font-size)
-;; (global-set-key (kbd "C--") 'my-dec-frame-font-size)
+;; (global-set-key (kbd "C-c C-+") 'hc-inc-frame-font-size)
+;; (global-set-key (kbd "C-c C-=") 'hc-inc-frame-font-size)
+;; (global-set-key (kbd "C-c C--") 'hc-dec-frame-font-size)
+
+(provide 'hc-alter-font-size)
+
+;;; hc-alter-font-size.el ends here
