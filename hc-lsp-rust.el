0e50562a7cae213fe82ec1e0f0e7e481168f6c1f
Harold Carr <harold.carr@gmail.com>
2022-12-02 08:37:20 -0700

    working on LSP setup

---
+++ hc-lsp-rust.el
@@ -0,0 +1,31 @@
+;;; hc-lsp-rust.el --- rust          -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; based on https://robert.kra.hn/posts/rust-emacs-setup/
+
+;;; Code:
+
+(use-package hc-lsp)
+
+(use-package lsp-mode
+  :ensure nil
+  :commands lsp
+  :custom
+  ;; (lsp-eldoc-render-all                                                 t)
+  ;; (lsp-idle-delay                                                       0.6)
+  ;; What to use when checking on-save. "check" is default.
+  (lsp-rust-analyzer-cargo-watch-command                                "clippy")
+  ;; This controls the overlays that display type and other hints inline.
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable              "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints                             t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints                  t)
+  (lsp-rust-analyzer-display-parameter-hints                            nil)
+  (lsp-rust-analyzer-display-reborrow-hints                             nil)
+  (lsp-rust-analyzer-server-display-inlay-hints                         t)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(provide 'hc-lsp-rust)
+
+;;; hc-lsp-rust.el ends here
--- hc-rust.el
+++ hc-rust.el
@@ -21,25 +21,7 @@
               ("C-c C-c h" . lsp-ui-doc-glance))
   )
 
-;; for rust-analyzer integration
-(use-package lsp-mode
-  :ensure nil
-  :commands lsp
-  :custom
-  ;; (lsp-eldoc-render-all                                                 t)
-  ;; (lsp-idle-delay                                                       0.6)
-  ;; What to use when checking on-save. "check" is default.
-  (lsp-rust-analyzer-cargo-watch-command                                "clippy")
-  ;; This controls the overlays that display type and other hints inline.
-  (lsp-rust-analyzer-display-lifetime-elision-hints-enable              "skip_trivial")
-  (lsp-rust-analyzer-display-chaining-hints                             t)
-  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
-  (lsp-rust-analyzer-display-closure-return-type-hints                  t)
-  (lsp-rust-analyzer-display-parameter-hints                            nil)
-  (lsp-rust-analyzer-display-reborrow-hints                             nil)
-  (lsp-rust-analyzer-server-display-inlay-hints                         t)
-  :config
-  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+(use-package hc-lsp-rust)
 
 ;; inline errors
 (use-package flycheck :ensure nil)
@@ -50,6 +32,6 @@
 ;; for Cargo.toml and other config files
 (use-package toml-mode :ensure nil)
 
-
 (provide 'hc-rust)
+
 ;;; hc-rust.el ends here
