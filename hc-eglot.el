4428d677cb522ee8e60162843e9820bfb864deed
Harold Carr <harold.carr@gmail.com>
2023-03-20 19:42:38 -0600

    fix eglot; more run-command

--- hc-eglot.el
+++ hc-eglot.el
@@ -4,23 +4,23 @@
 
 ;;;;
 ;;;; Created       : 2023 Mar 08 (Wed) 10:34:09 by Harold Carr.
-;;;; Last Modified : 2023 Mar 08 (Wed) 10:45:54 by Harold Carr.
+;;;; Last Modified : 2023 Mar 20 (Mon) 19:33:52 by Harold Carr.
 ;;;;
 
 ;;; Code:
 
 ;; https://joaotavora.github.io/eglot/
 
-(defvar hc-eglot-mode-keymap-prefix "C-c l")
+;;(defvar hc-eglot-mode-keymap-prefix "C-c l")
 
 (use-package eglot
   :config
-  (define-key eglot-mode-map (kbd hc-eglot-mode-keymap-prefix) eglot-mode-map)
+  ;;(define-key eglot-mode-map (kbd hc-eglot-mode-keymap-prefix) eglot-mode-map)
   :bind (:map eglot-mode-map
-              ("G r"   . xref-find-references)
-              ("h h"   . eldoc)
-              ("f b"   . flymake-show-buffer-diagnostics)
-              ("f p"   . flymake-show-project-diagnostics)
+              ("C-c l G r"   . xref-find-references)
+              ("C-c l h h"   . eldoc)
+              ("C-c l f b"   . flymake-show-buffer-diagnostics)
+              ("C-c l f p"   . flymake-show-project-diagnostics)
         )
 )
 
--- hc-run-command-package.el
+++ hc-run-command-package.el
@@ -4,6 +4,16 @@
 
 ;;; Code:
 
+;; a failed experiment to have no buffer and no output
+;; ;; https://macowners.club/posts/custom-functions-1-baseline/
+;; (defun timu-baseline-async-shell-command-no-window (command _buffer-base-name _output-buffer)
+;;   "Do not display the `async-shell-command' COMMAND output buffer.
+;; Credit: https://stackoverflow.com/a/60333836
+;; Credit: https://stackoverflow.com/a/47910509."
+;;   (interactive)
+;;   (let ((display-buffer-alist '(("\\*Async Shell Command\\*.*" display-buffer-no-window))))
+;;     (async-shell-command command)))
+
 (defun run-command-open ()
   (list
    (when-let ((filename (thing-at-point 'filename t)))
@@ -11,7 +21,13 @@
       :command-name "Open"
       :command-line (format "open '%s'" filename)
       :display (format "open '%s'" filename)
-      :runner 'run-command-runner-vterm))
+      :runner 'run-command-runner-compile))
+   (when-let ((filename (thing-at-point 'filename t)))
+     (list
+      :command-name "Quick Look"
+      :command-line (format "qlmanage -p '%s'" filename)
+      :display (format "qlmanage -p '%s'" filename)
+      :runner 'run-command-runner-compile))
   ))
 
 (use-package run-command
