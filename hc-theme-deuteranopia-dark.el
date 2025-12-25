f12b06af80e1dc66cf36cb330ef57f1adc077061
Harold Carr <harold.carr@gmail.com>
2025-12-25 11:52:49 -0700

    update

--- hc-theme-deuteranopia-dark.el
+++ hc-theme-deuteranopia-dark.el
@@ -1,4 +1,7 @@
 (eval-when-compile (require 'use-package))
-(use-package ef-deuteranopia-dark-theme :demand
-  :init (load-theme 'ef-deuteranopia-dark 'no-confirm))
+;; https://protesilaos.com/emacs/ef-themes#h:ac76ded0-af9b-4566-aff9-75142ef2d4ef
+(use-package ef-themes
+  :init   (ef-themes-take-over-modus-themes-mode 1)
+  :config (modus-themes-load-theme 'ef-deuteranopia-dark))
+
 
--- hc-theme-elea-dark.el
+++ hc-theme-elea-dark.el
@@ -1,2 +1,5 @@
 (eval-when-compile (require 'use-package))
-(use-package ef-elea-dark-theme :demand :init (load-theme 'ef-elea-dark 'no-confirm))
+;; https://protesilaos.com/emacs/ef-themes#h:ac76ded0-af9b-4566-aff9-75142ef2d4ef
+(use-package ef-themes
+  :init   (ef-themes-take-over-modus-themes-mode 1)
+  :config (modus-themes-load-theme 'ef-elea-dark))
